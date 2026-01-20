da7c1edbce3051f4860861deba7632fa9030f35f
Henry <henmon2018@gmail.com>
2026-01-19 22:39:43 -0500

    Cambio de logo: Ahora usa el escudo oficial del colegio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,9 +13,11 @@ const Header = () => {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link to="/" className="flex items-center">
-              <span className="text-2xl font-bold bg-linear-to-r from-indigo-900 to-amber-500 bg-clip-text text-transparent">
-                🎓 Premium School
-              </span>
+              <img 
+                src="https://scontent.fgye1-1.fna.fbcdn.net/v/t39.30808-6/343421420_198382112973960_4132014736359434467_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=lhzJFRyncdEQ7kNvwFwQRJU&_nc_oc=AdlHkPOfETozOCpq_6cExq0QC-1FWHvqmkoLXVhj894IGVb7MKcFG7R_CTZHNcKc6qjTraEEtIMQEZ7ITT-krYy4&_nc_zt=23&_nc_ht=scontent.fgye1-1.fna&_nc_gid=AoTieVxCgXiGnhw-QHSbgQ&oh=00_Afok2lvY7BdwNOKPx2gAjCPB7XP0bPXmbEziY0EHSSJKdg&oe=6974D30D"
+                alt="Logo Colegio Premium" 
+                className="h-14 w-auto"
+              />
             </Link>
           </div>
 
